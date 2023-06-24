924090bcb86a837065c1e187bfc7438ccb05dba2
MathiasGagnon <Mathias.Gagnon@USherbrooke.ca>
2023-06-24 18:58:55 -0400

    DataManager file creation and boiler plate code

---
+++ src/pipeline/data/DataManager.py
@@ -0,0 +1,20 @@
+class DataManager:
+    def __init__(self):
+        """
+        Initializes the DataManager.
+        """
+        # Add initialization code here
+
+    def concate_img(self, Images, size):
+        """
+        Concatenates multiple images together with the desired size.
+
+        Args:
+            Images (list): List of Image objects.
+            size (tuple): Desired size of the concatenated image.
+
+        Returns:
+            Image: The concatenated image.
+        """
+        # Add concatenation logic here
+        # Return the concatenated image
