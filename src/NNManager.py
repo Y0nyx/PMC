929a459c4b611a844651f6c44840d950b6299fee
Deutschpat <patqc4617@gmail.com>
2023-06-28 17:47:31 -0400

    Création de classe NNManager

---
+++ src/NNManager.py
@@ -0,0 +1,30 @@
+class NNManager:
+    instance = None
+
+    def __init__(self):
+        self.array_NeuralNetwork = []
+
+    @staticmethod
+    def get_instance():
+        if NNManager.instance is None:
+            NNManager.instance = NNManager()
+        return NNManager.instance
+
+    def predicts(self, features):
+        if not self.array_NeuralNetwork:
+            return None
+        return self.array_NeuralNetwork[0].predict(features)           # predict() est un placeholder pour la vraie method
+
+    def predicts(self, index_NN, features):
+        if index_NN < 0 or index_NN >= len(self.array_NeuralNetwork):
+            return None
+        return self.array_NeuralNetwork[index_NN].predict(features)    # predict() est un placeholder pour la vraie method
+
+    def trains(self):
+        for nn in self.array_NeuralNetwork:                            # Assomption que plusieurs NN seront entraînés en même temps
+            nn.train()                                                 # train() est un placeholder pour la vraie method
+
+    def trains(self, index_NN):
+        if index_NN < 0 or index_NN >= len(self.array_NeuralNetwork):
+            return
+        self.array_NeuralNetwork[index_NN].train()                     # train() est un placeholder pour la vraie method
