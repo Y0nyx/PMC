e053cc3c1eba7022076ceb341890d058b8f38fd5
Liv3lyJs <jeanseb0598@hotmail.com>
2024-01-15 10:03:01 -0500

    Adding folders for machine learning

---
+++ src/machine_learning/Un_supervised/Neural_Network/test_GAN.py
@@ -0,0 +1,38 @@
+#!/user/bin/env python
+
+"""
+Author: Jean-Sebastien Giroux
+Contributor(s): 
+Date: 01/11/2024
+Description: Test the performances of the GAN network on data it has never seen before. This will tell us the real performances
+              of the network and allow the team to know which hyper-parameters combinaisons are the best fit for the model. 
+              It will predict with a value between 0 and 1 if the analysed piece have a welding error or is error free.
+
+Note:         Once the network work for this two class classification problem, it can be interesting to add more class like the
+              one Charles made for it Yolo model. It could also be interesting to test different gan architectures and see which
+              one is the best fit for our situation. 
+"""
+
+#####################################################################################################################
+#                                                      Imports                                                      # 
+#####################################################################################################################
+
+
+
+#####################################################################################################################
+#                                                     Variables                                                     #
+#####################################################################################################################
+
+
+
+#####################################################################################################################
+#                                                     Functions                                                     #
+#####################################################################################################################
+
+
+
+######################################################################################################################
+#                                                        Main                                                        #
+######################################################################################################################
+if __name__ == '__main__':
+    pass
