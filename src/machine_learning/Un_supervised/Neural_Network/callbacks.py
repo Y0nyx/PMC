bdda0349f337c222eb3001ec19616f9f5428ac5d
Liv3lyJs <jeanseb0598@hotmail.com>
2024-01-25 14:46:36 -0500

    Adding callback function.

--- src/machine_learning/Un_supervised/Neural_Network/callbacks.py
+++ src/machine_learning/Un_supervised/Neural_Network/callbacks.py
@@ -1,36 +1,65 @@
-#!/user/bin/env python
-
-"""
-Author: Jean-Sebastien Giroux
-Contributor(s): 
-Date: 01/11/2024
-Description: List of callbacks that will be used during the training of the GAN. These callbacks are used to optimize 
-             the training of the Neural Network. Indeed they can change the outcome of the training by changing the 
-             loss function if the model haven't improve for a certain number of epochs, it can stop the training for 
-             the same reason mentionned earlier and finaly it can stop the training if NaN is obtained in the loss 
-             function. It is also interesting to note that the user can add as many custom callbacks as he likes. 
-"""
-
-#####################################################################################################################
-#                                                      Imports                                                      # 
-#####################################################################################################################
+import tensorflow as tf
 
 
+class TrainingCallbacks():
+    def __init__(self, filepath: str, monitor: str, mode: str, verbose: int):
+        self.filepath = filepath
+        self.monitor = monitor
+        self.mode = mode
+        self.verbose = verbose
 
-#####################################################################################################################
-#                                                     Variables                                                     #
-#####################################################################################################################
+    def best_model_weights(self) -> tf.keras.callbacks.Callback:
+        """
+        Modelcheckpoint callback to save the best model weights.
+        """
+        return tf.keras.callbacks.ModelCheckpoint(
+            filepath=self.filepath, 
+            monitor=self.monitor, 
+            save_best_only=True,
+            mode=self.mode,
+            save_weights_only=True, 
+            save_freq='epoch',
+            initial_value_threshold=None
+        )
 
+    def early_stop(self, min_delta: float=0.001, patience: int=15) -> tf.keras.callbacks.Callback:
+        """
+        EarlyStopping callback.
+        """
+        return tf.keras.callbacks.EarlyStopping(
+            monitor=self.monitor,
+            min_delta=min_delta,
+            patience=patience,
+            verbose=self.verbose,
+            mode=self.mode
+        )
 
+    def reduce_lr_plateau(self, factor: float=0.1, patience: int=10, min_delta: float=0.001) -> tf.keras.callbacks.Callback:
+        """
+        Reduce lr on plateau callback to reduce the learning rate when a plateau is detected.
+        """
+        return tf.keras.callbacks.ReduceLROnPlateau(
+            monitor=self.monitor, 
+            factor=factor,
+            patience=patience,
+            verbose=self.verbose,
+            mode=self.mode, 
+            min_delta=min_delta,
+            cooldown=0,
+            min_le=0
+        )
 
-#####################################################################################################################
-#                                                     Functions                                                     #
-#####################################################################################################################
+    def end_nan(self) -> tf.keras.callbacks.Callback:
+        """
+        TerminateOnNaN cakkback to terminate training when NaN loss is encountered. 
+        """
+        return tf.keras.callbacks.TerminateOnNaN()
 
-
-
-######################################################################################################################
-#                                                        Main                                                        #
-######################################################################################################################
-if __name__ == '__main__':
-    pass
+    def get_callbacks(self, min_delta_es: float=0.001, patience_es: int=15, factor_rp: float=0.1, 
+                      patience_rp: int=10, min_delta_rp: float=0.001) -> list:
+        return [
+            self.best_model_weights(),
+            self.early_stop(min_delta_es, patience_es),
+            self.reduce_lr_plateau(factor_rp, patience_rp, min_delta_rp),
+            self.end_nan()
+        ]
