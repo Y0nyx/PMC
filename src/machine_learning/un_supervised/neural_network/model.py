7e639f71d510d33b500d37b0c1fc215cccc113e8
Liv3lyJs <jeanseb0598@hotmail.com>
2024-02-07 23:31:09 -0500

    Mettre tous les modeles dans le meme code.

--- src/machine_learning/un_supervised/neural_network/model.py
+++ src/machine_learning/un_supervised/neural_network/model.py
@@ -12,6 +12,11 @@ from keras import layers
 from keras.layers import Conv2D, Conv2DTranspose, MaxPooling2D, BatchNormalization, LeakyReLU, Add, UpSampling2D, Activation
 import tensorflow as tf
 
+import numpy as np
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose, Activation
+from keras.models import Model
+from keras.preprocessing import image
+
 class AeModels():
     def __init__(self, learning_rate: float=0.001):
         self.learning_rate = learning_rate
@@ -61,6 +66,43 @@ class AeModels():
         x = Conv2D(1, (3,3), activation='sigmoid', padding='same')(x)
         #Autoencoder
         model = keras.Model(inputs=input, outputs=x)
+        opt = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
+        model.compile(
+            optimizer=opt, 
+            loss='binary_crossentropy',
+            metrics=['mean_absolute_error']
+        )
+
+        return model
+    
+    #TODO: Pass all parameters to tune
+    def build_basic_cae(self, input_shape: tuple=(128, 128, 3)) -> keras.Model:
+        """
+        Model based on this kaggle notebook: https://www.kaggle.com/code/orion99/autoencoder-made-easy/notebook
+        Tested using the subdivided dataset
+        """
+        input_layer = Input(shape=input_shape, name="INPUT")
+        x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_layer)
+        x = MaxPooling2D((2, 2))(x)
+        x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+        x = MaxPooling2D((2, 2))(x)
+        x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+
+        code_layer = MaxPooling2D((2, 2), name="CODE")(x)
+
+        x = Conv2DTranspose(8, (3, 3), activation='relu', padding='same')(code_layer)
+        x = UpSampling2D((2, 2))(x)
+        x = Conv2DTranspose(8, (3, 3), activation='relu', padding='same')(x)
+        x = UpSampling2D((2, 2))(x)
+        x = Conv2DTranspose(16, (3, 3), activation='relu', padding='same')(x)
+        x = UpSampling2D((2,2))(x)
+        output_layer = Conv2D(3, (3, 3), padding='same', name="OUTPUT")(x)
+        output_layer = Activation('sigmoid')(output_layer)
+
+         #Define the input and output of the model
+        model = Model(input_layer, output_layer)
+
+        #Create the loss function
         opt = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
         model.compile(
             optimizer=opt, 
