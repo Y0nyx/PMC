8732668cf3b219ad88935e847ac30cca26ab1257
Y0nyx <72567319+Y0nyx@users.noreply.github.com>
2024-03-25 13:38:30 -0400

    asyncio integration

--- src/NetworkManager.py
+++ src/NetworkManager.py
@@ -8,24 +8,29 @@ class NetworkManager():
     def __init__(self, worker: threading.Thread, host: str, port: str, verbose: bool = False):
         super().__init__()
         self.verbose = verbose
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self.loop)
-        self.s = None
 
         self.host = host
         self.port = port
 
         self.worker = worker
+
         self.executor = ThreadPoolExecutor()
+        self.loop = asyncio.get_event_loop()
         self.future = None
 
     def start(self):
-        self.loop.run_until_complete(self.run())
+        asyncio.ensure_future(self.run(), loop=self.loop)
+
+        try:
+            asyncio.get_event_loop().run_forever()
+        except KeyboardInterrupt:
+            print("Keyboard interrupt received, stopping...")
+        finally:
+            asyncio.get_event_loop().close()
     
     async def run(self):
         await self.socket_connect()
         await self.receive_message()
-
         await self.close_socket()
 
     async def receive_message(self) -> None:
@@ -36,14 +41,14 @@ class NetworkManager():
             self.print(f'Received code: {code}')
 
 
-            if self.future is not None and self.future.done():
+            if self.future and self.future.done():
                 self.print("NetworkManager : Finish Pipeline")
                 await self.send_message(self.future.result())
                 self.future = None
 
             if code == "start":
                 self.print("NetworkManager : Start Pipeline")
-                self.future = self.executor.submit(self.worker.start)
+                self.future = self.loop.run_in_executor(self.executor, self.worker.start)
             elif code ==  "stop":
                 self.print("NetworkManager : Stop Pipeline")
                 self.worker.stop()
@@ -53,18 +58,11 @@ class NetworkManager():
         serialized_data = json.dumps(data).encode()
         await self.loop.sock_sendall(self.s, serialized_data)
         self.print("Sent message")
-    
-    async def check_stop_signal(self) -> bool:
-        await asyncio.sleep(0)
-        if self.s.recv(1024, socket.MSG_PEEK) == b'':
-            return False
-        else:
-            await self.receive_message()
-            return True
         
     async def socket_connect(self) -> None:
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setblocking(False)
+
         connected = False
         while not connected:
             try:
