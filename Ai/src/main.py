deb0a0b43e67aa86bca25b9507847aa903ae71c9
Y0nyx <72567319+Y0nyx@users.noreply.github.com>
2024-09-25 14:54:54 -0400

    V1.1
    * merge fonctionne mtn
    
    * Updated paths
    
    * moved requirements
    
    ---------
    
    Co-authored-by: MachinePMCDofa <“dofapromo@gmail.com”>

--- Ai/src/main.py
+++ Ai/src/main.py
@@ -4,13 +4,6 @@ from Pipeline import Pipeline
 from common.Constants import *
 import tensorflow as tf
 
-def count_folders_starting_with(start_string, path):
-    count = 0
-    for root, dirs, files in os.walk(path):
-        for dir in dirs:
-            if dir.startswith(start_string):
-                count += 1
-    return count
 
 def path_initialization():
 
@@ -22,8 +15,7 @@ def path_initialization():
     unsupervised_model_path = f'{unsupervised_model_higher_path}{UNSUPERVISED_MODEL_REF}.keras'
 
     #Init path to current model iteration for logging purposes
-    current_iteration_latest_version_id = count_folders_starting_with(f"{UNSUPERVISED_MODEL_REF}", unsupervised_model_higher_path)
-    current_iteration_logging_path = f'/{UNSUPERVISED_MODEL_REF}_{current_iteration_latest_version_id}'
+    current_iteration_logging_path = f'{SAVE_RESULT}'
 
     return segmentation_model_path, unsupervised_model_path, current_iteration_logging_path
 
@@ -33,9 +25,8 @@ if __name__ == "__main__":
 
     segmentation_model_path, unsupervised_model_path, current_iteration_logging_path  = path_initialization()
 
-    supervised_models = [YoloModel(Path(segmentation_model_path))]
-    unsupervised_model = tf.keras.models.load_model(unsupervised_model_path)
+    segmentation_model = YoloModel(Path(segmentation_model_path))
 
-    pipeline = Pipeline(supervised_models=supervised_models, unsupervised_model=unsupervised_model, current_iteration_logging_path=current_iteration_logging_path)
+    pipeline = Pipeline(segmentation_model=segmentation_model, supervised_detection_model=None, unsupervised_model=None, current_iteration_logging_path=current_iteration_logging_path)
     networkManager = NetworkManager(pipeline, HOST, PORT, SUPERVISED_HOST, SUPERVISED_PORT, UNSUPERVISED_HOST, UNSUPERVISED_PORT, True)
     networkManager.start()
