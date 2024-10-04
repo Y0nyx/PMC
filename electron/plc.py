dcd0c8beaa704a60fcb62c6aba3c9adb3564e2b2
kabm0802 <72518882+kabm0802@users.noreply.github.com>
2024-10-04 19:39:47 -0400

    ajustement reception de donnée plc

--- electron/plc.py
+++ electron/plc.py
@@ -4,6 +4,10 @@ import pyftdi.serialext
 import asyncio
 import websockets
 import json
+import time
+
+writtingToPlc = 0
+
 class PCtoPLC:
 
     def __init__(self, url='ftdi:///1', baudrate=9600):
@@ -15,10 +19,12 @@ class PCtoPLC:
         self.port = pyftdi.serialext.serial_for_url(url, baudrate=baudrate)
         self.port.timeout = 0.05
         self.data = ''
+        self.isWriting = 0
 
     def write_to_plc(self, message):
 
-        try:
+        try:    
+            writtingToPlc = 1
             self.ftdi.set_cbus_gpio(0b1111)
             if type(message) is str:
                 message = str.encode(message)
@@ -28,13 +34,15 @@ class PCtoPLC:
                 pass
             else:
                 raise Exception("invalid type, str, int or bytes accepted for write_to_plc")
-
+            
             self.port.write(message)
+            writtingToPlc = 0
+
         except self.port.serialutil.SerialException:
-
-            self.read_from_plc()
+            writtingToPlc = 1
+            time.sleep(0.055)
             self.write_to_plc(message)
-
+            writtingToPlc = 0
         return 1
 
     def read_from_plc(self, size=8):
@@ -65,37 +73,39 @@ async def send_receive(plc,websocket):
 async def python_plc(plc,websocket):
 
     while True:
-        plc.read_from_plc()
-        #envoie moi le bon code mais a titre d'exemple
-        # Define a sample object to send
-        data = {}
-        if plc.data != b'':
-            print(plc.data)
-            if plc.data == 'R': 
-                data = 'ready'
-            
-            elif plc.data == 'E': 
-                data = 'error'
-        
-            data_to_send = {'code': 'start', 'data': data}
-            serialized_data = json.dumps(data_to_send)
-            await websocket.send(serialized_data)
+
+        if not writtingToPlc:
+            plc.read_from_plc()
+            #envoie moi le bon code mais a titre d'exemple
+            # Define a sample object to send
+            print(data)
+            data = {}
+            if plc.data != b'':
+                print(plc.data)
+                if b'R' in plc.data: 
+                    data = 'ready'
+                elif b'E' in plc.data:
+                    data = 'error'
+                data_to_send = {'code': 'start', 'data': data}
+                serialized_data = json.dumps(data_to_send)
+                await websocket.send(serialized_data)
 
 
 
 
 async def main():
- uri = "ws://127.0.0.1:8003"
- plc = PCtoPLC()
- async with websockets.connect(uri) as websocket:
-    # Schedule both send_receive and print_loop to run concurrently
-    pc_python_task = asyncio.create_task(send_receive(plc,websocket))
-    python_plc = asyncio.create_task(plc,python_plc(websocket))
+  uri = "ws://127.0.0.1:8003"
+  plc = PCtoPLC()
 
-    # Wait for both tasks to run indefinitely
-    await asyncio.gather(pc_python_task, python_plc)
+  async with websockets.connect(uri) as websocket:
+     # Schedule both send_receive and print_loop to run concurrently
+     pc_python_task = asyncio.create_task(send_receive(plc,websocket))
+     python_plc = asyncio.create_task(plc,python_plc(websocket))
 
-# Run the main function
+     # Wait for both tasks to run indefinitely
+     await asyncio.gather(pc_python_task, python_plc)
+
+# # Run the main function
 asyncio.run(main())
 
 
